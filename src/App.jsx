3c43cad3b61bc10b59b2d766249944860c8c60eb
SHussaing <husain187@hotmail.com>
2024-10-28 20:36:15 +0300

    changed from browserRouter to HashRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from './components/login';
 import Profile from './components/profile';
 import PrivateRoute from './components/privateRoute';
@@ -7,14 +7,9 @@ import NotFound from './components/notFound';
 import { ConfigProvider } from 'antd';
 
 function App() {
-    // Explicitly force '/' for localhost and use process.env.PUBLIC_URL for production
-    const isLocalhost = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1';
-    const basename = isLocalhost ? '/' : '/graphql';
-
-
     return (
         <ConfigProvider theme={{ token: { colorPrimary: '#3687d9' } }}> 
-            <Router basename={basename}>
+            <Router>
                 <Routes>
                     <Route path="/" element={<Login />} />
                     <Route 
